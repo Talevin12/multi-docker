ef6e878b78efbbb3065e6c9faee97628b20e3892
Talevin12 <talevin2000@gmail.com>
2023-12-15 19:04:04 +0100

    wake deployment

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React1
+            Learn React
           </a>
         </header>
       </div>
